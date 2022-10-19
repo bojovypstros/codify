74ca068cc96578ab1d403f98f03bdc32e5b5bff1
bojovypstros <root@matejkos.cz>
2022-10-19 12:48:06 +0200

    chore: prepare to publish

--- scripts/install.js
+++ scripts/install.js
@@ -10,9 +10,6 @@ const tar = require('tar-fs')
 const stream = require('stream')
 const gunzip = require('gunzip-maybe')
 
-/** Current zint git ref (specified in package.json) */
-const checkout = zintVersion || 'master'
-
 /**
  * Returns the absolute path to the given file in zint source.
  *
@@ -35,32 +32,4 @@ const createConfigFile = () => {
   fs.writeFileSync(getPath('backend/zintconfig.h'), fileData)
 }
 
-/**
- * Clones zint, then applies C source patches.
- */
-// const cloneAndPatch = async () => {
-//   console.log('Removing any existing .zint directory...')
-//
-//   rimraf(path.join(__dirname, '../.zint'), async () => {
-//     // console.log(`Cloning zint at '${checkout}' in`, path.join(__dirname, '../.zint'))
-//     fs.mkdir(path.join(__dirname, '../.zint'), async () => {
-//
-//       const res = await fetch('https://github.com/zint/zint/archive/refs/tags/2.11.1.tar.gz');
-//       const arrayBuffer = await res.arrayBuffer();
-//       const buffer = new Uint8Array(arrayBuffer);
-//
-//       await stream.Readable.from(buffer)
-//         .pipe(gunzip())
-//         .pipe(tar.extract(path.join(__dirname, '../.zint')));
-
-      // patches.forEach(patch => replace.sync(patch))
-
-      createConfigFile()
-
-//     })
-//   })
-// }
-//
-//
-// cloneAndPatch()
-
+createConfigFile()
